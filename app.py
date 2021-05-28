b86b39482e53e3c2ddc143d6ae4771e537b02730
Patrick <patrickanguyen1258@gmail.com>
2021-05-27 23:40:18 -0700

    Finished HTML template

--- app.py
+++ app.py
@@ -4,5 +4,19 @@ app = Flask(__name__)
 
 @app.get("/")
 def index():
-    user = "Patrick"
-    return render_template('index.html', user=user)
+    data = [
+        ("01-01-2020", 255),
+        ("01-05-2020", 55),
+        ("01-07-2020", 123),
+        ("01-08-2020", 66),
+        ("01-09-2020", 77),
+        ("01-10-2020", 56),
+        ("01-20-2020", 235),
+        ("01-16-2020", 600),
+    ]
+
+    labels = [row[0] for row in data]
+    values = [row[1] for row in data]
+
+    return render_template('index.html', fullness_labels=labels, fullness_values=values, temp_labels=labels, temp_values=values)
+    
